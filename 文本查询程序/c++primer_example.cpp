91d02786b0d3ebcfe0880eeedbd19b29c4f324ae
TColorCat <2801714044@qq.com>
2017-01-14 21:54:09 +0800

    19.3 dynamic_cast的问题

--- 文本查询程序/c++primer_example.cpp
+++ 文本查询程序/c++primer_example.cpp
@@ -2,18 +2,13 @@
 //
 
 #include "stdafx.h"
-
+#include"193P.h"
 #include"textquery.h"
+using namespace P193s;
 int _tmain(int argc, _TCHAR* argv[])
 {
-	std::ifstream is(argv[1]);
-	TextQuery t1(is);
-	std::string word;
-	Query q = Query("fiery") & Query("bird") | Query("wind");
-	//Query q = Query("fiery");  
-	//Query q = Query("A");  
-	std::cout << q << std::endl;
-	std::cout << q.eval(t1) << std::endl;
+	
+	
 	return 0;
 }
 
